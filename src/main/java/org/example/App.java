295aa8e7f0ca812e0dc9b927ad833bb1a02b88b5
fjux <erikalfredsson1@gmail.com>
2021-10-12 12:40:08 +0200

    Commit with a switch calculator that now has content and a catch for "division by zero" exception

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -36,15 +36,59 @@ public class App
 
         switch (operator) {
             case '+':
+                System.out.println("Enter the numbers you want to add together. If you enter 0 the addition ends.");
+                Scanner input1 = new Scanner(System.in);
+                int number;
+                int result = 0;
+                do {
+                    number = input1.nextInt(); //This creates a variable called number that stores the user input.
+
+                    result+= number; //This uses the variable result and adds it to the variable number.
+
+                } while(number != 0); //As long as the number inserted by the user isn't zero(0) the loop continues.
+
+                System.out.println("Sum is : " + result);
                 break;
 
             case '-':
+                System.out.println("Enter the numbers you want to subtract from each other. If you enter 0 the addition ends.");
+                Scanner input2 = new Scanner(System.in);
+                int number1;
+                int result1 = 0;
+                do {
+                    number1 = input2.nextInt(); //This creates a variable called number that stores the user input.
+
+                    result1-= number1; //This uses the variable result and subtracts it from the variable number.
+
+                } while(number1 != 0); //As long as the number inserted by the user isn't zero(0) the loop continues.
+
+                System.out.println("The difference is : " + result1);
                 break;
 
             case '*':
+                System.out.println("Enter the first number you want to multiply");
+                int number2 = input.nextInt();
+
+                System.out.println("Enter the second number you want to multiply");
+                int number3 = input.nextInt();
+
+                int result2 = number2 * number3;
+                System.out.println("The product is : " + result2);
                 break;
 
             case '/':
+                System.out.println("Enter the first number you want to divide");
+                int number4 = input.nextInt();
+
+                System.out.println("Enter the second number you want to divide");
+                int number5 = input.nextInt();
+                try {
+                    int result3 = number4 / number5;
+                    System.out.println("The quotient is : " + result3);
+                }
+                catch (Exception e){
+                    System.out.println("You can't divide by zero (0)");
+                }
                 break;
 
             default:
