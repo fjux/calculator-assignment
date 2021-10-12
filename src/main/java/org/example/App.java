1985816a5ac369830e86a10b4e7c0e426a010af4
fjux <erikalfredsson1@gmail.com>
2021-10-12 11:34:13 +0200

    Commit with do/while-loop for index.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        boolean isRunning = true;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Welcome co the calculator! \n \n 1. Start the calculations \n \n 2. Quit the program \n \n");
+            System.out.print("To continue enter a number from the index:");
+            int index = input.nextInt();
+            switch (index) {
+                case 1:
+
+                    break;
+
+                case 2:
+                    isRunning = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid input!");
+                    break;
+
+            }
+        }while(isRunning);
+        System.out.print("You have closed the calculator. \nGood bye!");
+
     }
 }
